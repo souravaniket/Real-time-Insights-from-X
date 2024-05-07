72ae99cec3e50fc5366d178b2cb9398c3cf16c79
souravaniket <91434551+souravaniket@users.noreply.github.com>
2024-05-07 16:01:01 +0530

    Update pr1.py

--- pr1.py
+++ pr1.py
@@ -7,10 +7,6 @@ from collections import Counter
 WW_trends = json.loads(open(r'C:\Users\Aniket Sourav\Downloads\WWTrends.json').read())
 US_trends = json.loads(open(r'C:\Users\Aniket Sourav\Downloads\USTrends.json').read())
 
-# Pretty-printing the results. First WW and then US trends.
-print("WW trends:", WW_trends)
-print("\n", "US trends:", US_trends)
-
 # Extracting all the WW trend names from WW_trends
 world_trends = set([trend['name'] for trend in WW_trends[0]['trends']])
 # Extracting all the US trend names from US_trends
@@ -18,14 +14,10 @@ us_trends = set([trend['name'] for trend in US_trends[0]['trends']])
 # Getting the intersection of the two sets of trends
 common_trends = world_trends.intersection(us_trends)
 # Inspecting the data
-print(world_trends, "\n")
-print(us_trends, "\n")
-print(len(common_trends), "common trends:", common_trends)
+print("Common trends:", common_trends)
 
 # Loading the data
 tweets = json.loads(open(r"C:\Users\Aniket Sourav\Downloads\WWTrends.json").read())
-# Inspecting some tweets
-tweets[0:2]
 
 # Extracting the text of all the tweets from the tweet object
 texts = [tweet['text'] for tweet in tweets if 'text' in tweet]
@@ -37,17 +29,15 @@ names = [user_mention['screen_name'] for tweet in tweets if 'entities' in tweet 
 hashtags = [hashtag['text'] for tweet in tweets if 'entities' in tweet for hashtag in tweet['entities'].get('hashtags', [])]
 
 # Inspecting the first 10 results
-print(json.dumps(texts[0:10], indent=1), "\n")
-print(json.dumps(names[0:10], indent=1), "\n")
-print(json.dumps(hashtags[0:10], indent=1), "\n")
+print("Texts:", json.dumps(texts[0:10], indent=1))
+print("Names:", json.dumps(names[0:10], indent=1))
+print("Hashtags:", json.dumps(hashtags[0:10], indent=1))
 
-# Counting occurrences/getting frequency dist of all names and hashtags
-name_counter = Counter(names)
-hashtag_counter = Counter(hashtags)
-
-# Inspecting the 10 most common items in the counters
-print(name_counter.most_common(10), "\n")
-print(hashtag_counter.most_common(10), "\n")
+# Counting occurrences/ getting frequency dist of all names and hashtags
+for item in [names, hashtags]:
+    c = Counter(item)
+    # Inspecting the 10 most common items in c
+    print("Most common items:", c.most_common(10))
 
 retweets = [
     (tweet['retweet_count'],
@@ -62,15 +52,22 @@ retweets = [
 # Create a DataFrame and visualize the data in a pretty and insightful format
 df = pd.DataFrame(retweets, columns=['Retweets', 'Favorites', 'Followers', 'ScreenName', 'Text']).groupby(['ScreenName', 'Text', 'Followers']).sum().sort_values(by=['Followers'], ascending=False)
 
-df.style.background_gradient()
-
 # Extracting language for each tweet and appending it to the list of languages
 tweets_languages = [tweet['lang'] for tweet in tweets if 'lang' in tweet]
 
-# Extracting source for each tweet and appending it to the list of sources
+# Extracting sources for each tweet and appending it to the list of sources
 tweets_sources = [tweet['source'] for tweet in tweets if 'source' in tweet]
 
 # Plotting the distribution of languages
 plt.hist(tweets_languages)
+plt.title('Distribution of Languages')
+plt.xlabel('Languages')
+plt.ylabel('Frequency')
+plt.show()
+
+# Plotting the distribution of tweet sources
 plt.hist(tweets_sources)
+plt.title('Distribution of Tweet Sources')
+plt.xlabel('Tweet Sources')
+plt.ylabel('Frequency')
 plt.show()
